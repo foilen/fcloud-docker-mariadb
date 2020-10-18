75b596462ed738b33b65c79adffa642a84c79854
Simon Levesque <simon@foilen.com>
2020-10-17 22:13:57 -0400

    Ensure the application is well shutdown before letting the script end

--- assets/mariadb-start.sh
+++ assets/mariadb-start.sh
@@ -4,72 +4,79 @@ rm -f /var/lib/mysql/CAN_USE
 
 set -e
 
-# Initializing
-if [ ! -f /var/lib/mysql/READY ]; then
-  echo MariaDB - Initializing the DB
-  /usr/bin/mysql_install_db --auth-root-authentication-method=normal
-  
-fi
+export MYSQL_ROOT_PASSWORD=$(cat /newPass)
 
 # Start
 echo MariaDB - Starting
-/usr/sbin/mysqld &
+/usr/local/bin/docker-entrypoint.sh mysqld &
 APP_PID=$!
-echo MariaDB - Started
-sleep 5
 
 # Change root password if needed
-LAST_PASS=$(cat /volumes/config/lastPass || echo)
 NEW_PASS=$(cat /newPass)
-if [ "$LAST_PASS" != "$NEW_PASS" ]; then
-  echo MariaDB - Update the password
-  
-  if [ "$LAST_PASS" == "" ]; then
-    echo MariaDB- Had no password 
-    mysql -u root -h 127.0.0.1 << _EOF
+if [ -f /volumes/config/lastPass ]; then
+
+  LAST_PASS=$(cat /volumes/config/lastPass 2>/dev/null || echo)
+  if [ "$LAST_PASS" != "$NEW_PASS" ]; then
+    echo MariaDB - Update the password
+    sleep 5s
+    
+    if [ "$LAST_PASS" == "" ]; then
+      echo MariaDB- Had no password 
+      mysql -u root -h 127.0.0.1 << _EOF
 DROP USER IF EXISTS root@localhost;
 DROP USER IF EXISTS root@'127.0.0.1';
 DROP USER IF EXISTS root@'::1';
 GRANT ALL PRIVILEGES ON *.* TO root@'%' IDENTIFIED BY '$NEW_PASS' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 _EOF
-  else
-    echo MariaDB - Had a password
-    mysql --defaults-file=/volumes/config/lastPass.cnf -u root -h 127.0.0.1 << _EOF
+    else
+      echo MariaDB - Had a password
+      mysql --defaults-file=/volumes/config/lastPass.cnf -u root -h 127.0.0.1 << _EOF
 DROP USER IF EXISTS root@localhost;
 DROP USER IF EXISTS root@'127.0.0.1';
 DROP USER IF EXISTS root@'::1';
 GRANT ALL ON *.* TO root@'%' IDENTIFIED BY '$NEW_PASS';
 FLUSH PRIVILEGES;
 _EOF
+    fi
+
   fi
-  echo $NEW_PASS > /volumes/config/lastPass
-  cp /newPass.cnf /volumes/config/lastPass.cnf
+
 fi
+echo $NEW_PASS > /volumes/config/lastPass
+cp /newPass.cnf /volumes/config/lastPass.cnf
+
+# Wait that it is ready
+echo MariaDB - Wait for it to be accessible
+while ! echo SHOW DATABASES | mysql --defaults-file=/newPass.cnf -u root -h 127.0.0.1 > /dev/null 2> /dev/null ; do
+  sleep 1s
+done
 
 # Run the upgrade if the version changed
-LAST_VERSION=$(cat /volumes/config/lastversion || echo)
+LAST_VERSION=$(cat /volumes/config/lastversion 2> /dev/null || echo)
 NEW_VERSION=$(mysql --version)
 if [ "$LAST_VERSION" != "$NEW_VERSION" ]; then
   echo MariaDB - Upgrade the database
-  mysql_upgrade --defaults-file=/volumes/config/lastPass.cnf -u root -h 127.0.0.1
+  mysql_upgrade --defaults-file=/newPass.cnf -u root -h 127.0.0.1
   
   echo "$NEW_VERSION" > /volumes/config/lastversion
 fi
 
+# Fully ready
 echo MariaDB - Ready to use
 
 touch /var/lib/mysql/READY
 touch /var/lib/mysql/CAN_USE
 
-echo MariaDB - Ensure innodb_fast_shutdown = 0
-mysql --defaults-file=/volumes/config/lastPass.cnf -u root -h 127.0.0.1 << _EOF
-  SET GLOBAL innodb_fast_shutdown = 0;
-_EOF
-
+# Wait for a SIGTERM
 function softQuit {
   echo MariaDB - Got a SIGTERM signal. Sending mysqladmin shutdown command
-  mysqladmin --defaults-file=/volumes/config/lastPass.cnf -u root -h 127.0.0.1 shutdown
+  mysqladmin --defaults-file=/newPass.cnf -u root -h 127.0.0.1 shutdown
+
+  while [ -d "/proc/$APP_PID" ]; do
+    sleep 1s
+  done
+  echo MariaDB - Fully stopped
 }
 
 trap softQuit SIGTERM
